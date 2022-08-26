e95d38258311e099143f25808055529664196dd1
Leif <zmeriksen@icloud.com>
2022-08-26 13:30:14 -0600

    Initial Commit

---
+++ ForkedArrayPicturesExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForkedArrayPicturesExample
+//
+//  Created by Leif on 8/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ForkedArrayPicturesExample/ForkedArrayPicturesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForkedArrayPicturesExampleApp.swift
+//  ForkedArrayPicturesExample
+//
+//  Created by Leif on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ForkedArrayPicturesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
